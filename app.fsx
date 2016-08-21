2703ed36586a9f0dcc0471ab858c3a38b83fbe78
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2016-08-21 18:32:52 -0400

    use latest packet

--- app.fsx
+++ app.fsx
@@ -1,6 +1,6 @@
 #if BOOTSTRAP
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-if not (System.IO.File.Exists "paket.exe") then let url = "https://github.com/fsprojects/Paket/releases/download/2.40.9/paket.exe" in use wc = new System.Net.WebClient() in let tmp = System.IO.Path.GetTempFileName() in wc.DownloadFile(url, tmp); System.IO.File.Move(tmp,System.IO.Path.GetFileName url);;
+if not (System.IO.File.Exists "paket.exe") then let url = "https://github.com/fsprojects/Paket/releases/download/3.13.3/paket.exe" in use wc = new System.Net.WebClient() in let tmp = System.IO.Path.GetTempFileName() in wc.DownloadFile(url, tmp); System.IO.File.Move(tmp,System.IO.Path.GetFileName url);;
 #r "paket.exe"
 Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
 #endif
